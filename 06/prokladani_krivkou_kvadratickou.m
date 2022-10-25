b649db5497808d74a04f9c0f10a8271fcaa5988a
Tomas Halada <halada.tms@gmail.com>
2022-10-25 14:38:33 +0200

    cv6 - fix

--- 06/prokladani_krivkou_kvadratickou.m
+++ 06/prokladani_krivkou_kvadratickou.m
@@ -6,8 +6,8 @@ grid on
 print -dpng kvadraticka_funkce_body.png
 
 A = [ 5, 0, 2.5;
-      0, 2.5, 2.250;
-      2.5, 0.250, 2.125;];
+      0, 2.5, 0;
+      2.5, 0., 2.125;];
 
 b = [ sum(y), sum(y.*x), sum(y.*x.*x) ]
 
@@ -16,12 +16,12 @@ kvadraticka_funkce = @(x) koef(1) + koef(2)*x + koef(3)*x.*x
 
 x_p = [x(1) : 0.01 : x(end)];
 
+
 hold on
 plot(x, y, 'ok')
 plot(x_p, kvadraticka_funkce(x_p), 'k', 'LineWidth',5)
 grid on
 print -dpng kvadraticka_funkce.png
 
-% -------------------------------------------------------------------------------
 
 
