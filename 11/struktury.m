50e5bd88e53a353fee39ad120fa173e0318e2bc4
tomashalada <halada.tms@gmail.com>
2024-11-20 10:24:21 +0100

    Poznamky struktura.

--- 11/struktury.m
+++ 11/struktury.m
@@ -1,18 +1,18 @@
 %%Struktury
 
 % NAZEV_STRUKTURY = struct( 'nazevPrvku', hodnota,
-%														'nazevPrvku', hodnota,
-%                           .. )
+%                           'nazevPrvku', hodnota,
+%                            .. )
 
 prvniMereni = struct( 'teplota', 22.6,
-								 			'casy', [0, 0.21, 0.54, 0.66, 0.69, 0.88, 1.03],
-								 			'mereniHodnoty', [111, 112.22, 114.43, 114.86, 147.23, 189.1, 201.3],
-								 			'titulek', "first measurement")
+							 'casy', [0, 0.21, 0.54, 0.66, 0.69, 0.88, 1.03],
+							 'mereniHodnoty', [111, 112.22, 114.43, 114.86, 147.23, 189.1, 201.3],
+							 'titulek', "first measurement")
 
 prvniMereni %nebo %disp(prvniMereni)
 
 disp( prvniMereni.teplota )            % pristup k prvkum struktury
-disp( prvniMereni.mereniHodnoty ) 			% pristup k prvkum struktury
+disp( prvniMereni.mereniHodnoty ) 	   % pristup k prvkum struktury
 
 disp("__________________________________________________")
 
@@ -32,18 +32,16 @@ disp("__________________________________________________")
 
 % POLE STRUKTUR
 % NAZEV_STRUKTURY = struct( 'nazevPrvku', { hodnota, hodnota... }
-%														'nazevPrvku', { hodnota, hodnota... }
-%                           .. )
-
+%                           'nazevPrvku', { hodnota, hodnota... }
+%                            .. )
 
 mereni = struct( 'teplota', { 22.6, 23.6 },
-								 'casy', {[0, 0.21, 0.54, 0.66, 0.69, 0.88, 1.03], [0, 0.21, 0.54, 0.66, 0.69, 0.88, 1.03]},
-								 'mereniHodnoty', {[111, 112.22, 114.43, 114.86, 147.23, 189.1, 201.3], [111, 112.22, 114.43, 114.86, 147.23, 189.1, 201.3]},
-								 'titulek', { "first measurement" ,"second measurement" , });
+                 'casy', {[0, 0.21, 0.54, 0.66, 0.69, 0.88, 1.03], [0, 0.21, 0.54, 0.66, 0.69, 0.88, 1.03]},
+                 'mereniHodnoty', {[111, 112.22, 114.43, 114.86, 147.23, 189.1, 201.3], [111, 112.22, 114.43, 114.86, 147.23, 189.1, 201.3]},
+                 'titulek', { "first measurement" ,"second measurement" , });
 
 disp( 'Vypis z druhe struktury:' )
 disp( mereni(2) )
-
 
 disp( 'Vypis z casu z prvni struktury:' )
 disp( mereni(1).casy )
@@ -56,13 +54,13 @@ fieldnames(druheMereni)
 disp( 'Ziskej datove pole ze struktury:' )
 getfield(druheMereni, 'casy')
 
-
 disp("__________________________________________________")
 
 pole = 'f';
 hodnoty = {'some text';
-         [10, 20, 30];
-         magic(5)};
+           [10, 20, 30];
+           magic(5)};
 
 struktura = struct( pole, hodnoty )
 disp( 'Vypis pole struktury:' )
+disp( struktura(1).f )
